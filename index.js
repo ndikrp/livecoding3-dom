cb11b6cf8a44636a501a4e8829144ededff9129e
ndikrp <andhikarizky36@gmail.com>
2024-03-05 22:07:23 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var title = document.firstElementChild.lastElementChild.firstElementChild
+title.innerHTML = "Hello FSW 1 Luar Biasa!!"
+document.getElementsByTagName("li")[0].style.color = "red"
+document.getElementsByTagName("li")[1].style.color = "green"
+var div = document.createElement("div")
+div.innerHTML =  "<h1>Hello Semua</h1>"
+document.querySelector("body").appendChild(div)
